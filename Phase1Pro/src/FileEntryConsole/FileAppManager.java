110279a2a3e5d458ae13379415027f1c37311ed3
Ajunu <ajunumasa@gmail.com>
2020-10-04 00:10:29 -0700

    Added Flowchart and project documentation files

--- Phase1Pro/src/FileEntryConsole/FileAppManager.java
+++ Phase1Pro/src/FileEntryConsole/FileAppManager.java
@@ -23,25 +23,27 @@ public class FileAppManager {
             System.out.println("Choose from 1 to 5 and press Enter to perform the below operations");
             System.out.println("");
             System.out.println("1-Display Files in Sorted order");
-            System.out.println("2-Search For a File");
+            System.out.println("2-Add a New File");
             System.out.println("3-Delete File");
-            System.out.println("4-Add a New File");
+            System.out.println("4-Search For a File");
             System.out.println("5-Exit");
 
             optionChoosen = scChoice.nextInt();
             switch (optionChoosen) {
+            
                 case 1:
                     displayFile obj = new displayFile();
                     obj.displayFileFunc("root");
                     break;
-                case 2:
-                    System.out.println("Enter the File Name to Search");
-                    Scanner scSearch = new Scanner(System.in);
-                    String FileToSearch = scSearch.nextLine();
                     
-                    searchFile obj2 = new searchFile();
-                    obj2.searchFile("root", FileToSearch.trim());
+                case 2:
+                    System.out.println("Enter the File Name to Add");
+                    Scanner scAdd = new Scanner(System.in);
+                    String FileNameToAdd = scAdd.nextLine();
+                    addFile obj4=new addFile();
+                    obj4.AddFileFunc("root",FileNameToAdd.trim());
                     break;
+                    
                 case 3:
                     System.out.println("Enter the File Name to Delete");
                     Scanner scDel = new Scanner(System.in);
@@ -50,19 +52,22 @@ public class FileAppManager {
                     deleteFile obj3 = new deleteFile();
                     obj3.deleteFileFunc("root", FileToDelete.trim());
                     break;
+                    
                 case 4:
-                   System.out.println("Enter the File Name to Add");
-                   Scanner scAdd = new Scanner(System.in);
-                   String FileNameToAdd = scAdd.nextLine();
-                   
-                    addFile obj4=new addFile();
-                    obj4.AddFileFunc("root",FileNameToAdd.trim());
+                    System.out.println("Enter the File Name to Search");
+                    Scanner scSearch = new Scanner(System.in);
+                    String FileToSearch = scSearch.nextLine();
+                    
+                    searchFile obj2 = new searchFile();
+                    obj2.searchFile("root", FileToSearch.trim());
                     break;
+               
                 case 5:
                     exit = true;
+                    System.out.println("Thank you for using Lockers Pvt.com SEE YOU SOON!!");
                     break;
                 default:
-                    System.out.println("Choose the Correct option from  1 to 5 to perform the below operations");
+                    System.out.println("Incorrect option : Choose the option from  1 to 5 to perform the below operations");
                     exit = true;
                     break;
             }
